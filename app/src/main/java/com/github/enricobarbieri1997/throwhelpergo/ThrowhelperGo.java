c9be0d21f92d0a1dbbd4965d90eec6d24df847dd
vonbicycle <a@b.c>
2016-07-26 15:45:31 +0200

    Project setup

---
+++ app/src/main/java/com/github/enricobarbieri1997/throwhelpergo/ThrowhelperGo.java
@@ -0,0 +1,13 @@
+package com.github.enricobarbieri1997.throwhelpergo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThrowhelperGo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_throwhelper_go);
+    }
+}
